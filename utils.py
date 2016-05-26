b14c969db12a49064724a056f4d6651f88bef6e1
Abhinav <abhinavkashyap92@gmail.com>
2016-05-26 18:54:40 +0800

    finds euclidean distance without any loops

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def get_euclidean_similarity(data):
+def get_euclidean_similarity(data, square=True):
     """
         Very naive implementation of getting the similarity matrix of data
         ARGS:
@@ -16,11 +16,17 @@ def get_euclidean_similarity(data):
     """
     N, M = data.shape
     data = data.astype('float') # just making sure that it is float
+    data_copy = data.copy()
     similarity = np.zeros((N, N))
-    for i in xrange(N):
-        for j in xrange(N):
-            if i is not j:
-                similarity[i][j] = np.linalg.norm(data[i]-data[j])
+
+    data_square = np.sum(np.square(data), axis=1)
+    data_copy_square = np.sum(np.square(data_copy), axis=1)
+    multiply = np.dot(data, data_copy.T)
+    similarity = data_square[:, np.newaxis] + data_copy_square - (2 * multiply)
+
+    if not square:
+        similarity = np.sqrt(similarity)
+
 
     # Using the masked array to find the median of the similarities
     masked_array = np.zeros((N, N), dtype='Bool')
@@ -35,5 +41,5 @@ if __name__ == "__main__":
     # Testing the euclidean similarity with simple matrices
     data = np.array([[1, 2, 3, 4], [5, 6, 7, 8]], dtype='float')
     correct_output = np.array([[8.0, 8.0], [8.0, 8.0]])
-    euclidean_similarity = get_euclidean_similarity(data)
+    euclidean_similarity = get_euclidean_similarity(data, square=False)
     print "Are the two arrays equal %s" % (np.allclose(euclidean_similarity, correct_output),)
